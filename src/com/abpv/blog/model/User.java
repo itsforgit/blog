286a87f3a8b5acfcabef0cef0d30b8bd0b4f8dbf
vivek <vivekooty2005@gmail.com>
2011-10-14 18:07:14 +0530

    vivek edited the code

--- src/com/abpv/blog/model/User.java
+++ src/com/abpv/blog/model/User.java
@@ -5,16 +5,15 @@ package com.abpv.blog.model;
 
 /**
  * This class is demo
+ * 
  * @author pirabu
  * 
  */
 public class User {
 
 	static {
-		System.out.println("Welcome Boss !");
-		System.out.println("Hello World");
-		System.out.println("Welcome to Git");
-		
+		System.out.println("vivek rocks");
+
 	}
 
 }
