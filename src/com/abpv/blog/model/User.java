a3835ae22c4f19ab972ccee6a928b70eea8f04c0
vivek <vivekooty2005@gmail.com>
2011-10-14 16:42:55 +0530

    applied something

--- src/com/abpv/blog/model/User.java
+++ src/com/abpv/blog/model/User.java
@@ -4,12 +4,17 @@
 package com.abpv.blog.model;
 
 /**
+ * This class is demo
  * @author pirabu
- *
+ * 
  */
 public class User {
-	static{
-		System.out.println("Welcome Boss");
+
+	static {
+		System.out.println("Welcome Boss !");
+		System.out.println("Hello World");
+		System.out.println("jdhgfjgdfg");
+		
 	}
 
 }
